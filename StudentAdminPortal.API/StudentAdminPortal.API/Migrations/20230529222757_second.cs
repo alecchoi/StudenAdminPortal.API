45600b37653a5c25e2df0d4f6205b5ca72c1c6d4
alecchoi <alec.choi8@gmail.com>
2023-05-30 11:38:01 -0400

    End of Section 2 - API

---
+++ StudentAdminPortal.API/StudentAdminPortal.API/Migrations/20230529222757_second.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StudentAdminPortal.API.Migrations
+{
+    public partial class second : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MyProperty",
+                table: "Student",
+                newName: "ProfileImageUrl");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProfileImageUrl",
+                table: "Student",
+                newName: "MyProperty");
+        }
+    }
+}
